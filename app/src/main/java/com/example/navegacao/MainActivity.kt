695ed91c75f653cb6cd41ebfdebb075153f2683b
Gustavo Wagner <gugawag@gmail.com>
2024-08-06 11:19:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/navegacao/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.navegacao
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navegacao.ui.telas.TelaLogin
+import com.example.navegacao.ui.telas.TelaPrincipal
+import com.example.navegacao.ui.theme.NavegacaoTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavegacaoTheme {
+                Scaffold(
+                    topBar = {
+                    TopAppBar(
+                        colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                        title = {
+                            Text("Login")
+                        },
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }, modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "login") {
+                        composable("login") {
+                            TelaLogin(modifier = Modifier.padding(innerPadding), onSignInClick = {
+                                navController.navigate("principal")
+                            })
+                        }
+                        composable("principal") {
+                            TelaPrincipal(modifier = Modifier.padding(innerPadding), onSignUpClick = {
+                                navController.navigate("login")
+                            })
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    TelaLogin(modifier, onSignInClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavegacaoTheme {
+        TelaLogin(onSignInClick = {})
+    }
+}
